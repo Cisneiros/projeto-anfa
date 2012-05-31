e1bb3d24b4d2707acdbfe79bd9a27ca8095188a8
Cisneiros <alexandre@cisneiros.com>
2012-05-31 18:08:05 -0300

    Some Javadoc.

--- AwesomeDropbox/src/br/ufpe/cin/anfa/net/Socket.java
+++ AwesomeDropbox/src/br/ufpe/cin/anfa/net/Socket.java
@@ -11,10 +11,21 @@ public class Socket {
 	Buffer receiveBuffer;
 	long readBease;
 	
+	/**
+	 * 
+	 * @param data The bytes to be sent.
+	 * @throws BufferException When the buffer is full.
+	 */
 	public void sendData(byte[] data) throws BufferException {
 		sendBuffer.insertData(data);
 	}
 	
+	/**
+	 * 
+	 * @param ammount How many bytes to be read.
+	 * @return The data requested.
+	 * @throws BufferException When something weird happens.
+	 */
 	public byte[] receiveData(long ammount) throws BufferException {
 		byte[] data = receiveBuffer.getData(readBease, readBease+ammount);
 		readBease += ammount;
